fd4963a1b56fef14e7ab6106ae0c8ee12c928e97
Sai Rama Doddi <sairama1026@gmail.com>
2025-11-17 21:46:05 +0530

    remove uselocation declarattion

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Services from "./components/Services";
